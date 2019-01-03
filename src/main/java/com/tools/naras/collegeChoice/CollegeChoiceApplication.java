a0b93c0db5ee9d6ff5c0e1d41ce874866b5a864b
Sahya <sahyanara@Sahyas-MBP.fios-router.home>
2019-01-02 21:20:38 -0500

    Initial commit

---
+++ src/main/java/com/tools/naras/collegeChoice/CollegeChoiceApplication.java
@@ -0,0 +1,14 @@
+package com.tools.naras.collegeChoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeChoiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tools/naras/collegeChoice/CollegeChoiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tools.naras.collegeChoice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollegeChoiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
